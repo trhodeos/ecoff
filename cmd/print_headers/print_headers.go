5afd38ac74538c1b66781c4978385d35edb7d7da
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-02-26 16:40:41 -0800

    First pass of ecoff header parser.

---
+++ cmd/print_headers/print_headers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+  "encoding/binary"
+	"fmt"
+	"os"
+	"github.com/trhodeos/ecoff"
+)
+
+func main() {
+  if len(os.Args) != 2 {
+    fmt.Printf("%s <filepath>\n", os.Args[0])
+    fmt.Printf("  filepath - path to big endian ECOFF file.\n")
+    os.Exit(1)
+  }
+  path := os.Args[1]
+
+  file, err := os.Open(path)
+  if err != nil { panic(err) }
+  defer file.Close()
+
+  fmt.Printf("%s opened\n", path)
+  header, err := ecoff.ParseHeader(file, binary.BigEndian)
+  if err != nil { panic(err) }
+  fmt.Printf("Parsed header from %s:\n%+v\n", path, header)
+}
