e54570a0fac23c0fa7f605681345611f345ce0f6
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-02-28 16:45:34 -0800

    Run gofmt

--- cmd/print_headers/print_headers.go
+++ cmd/print_headers/print_headers.go
@@ -1,26 +1,30 @@
 package main
 
 import (
-  "encoding/binary"
+	"encoding/binary"
 	"fmt"
-	"os"
 	"github.com/trhodeos/ecoff"
+	"os"
 )
 
 func main() {
-  if len(os.Args) != 2 {
-    fmt.Printf("%s <filepath>\n", os.Args[0])
-    fmt.Printf("  filepath - path to big endian ECOFF file.\n")
-    os.Exit(1)
-  }
-  path := os.Args[1]
+	if len(os.Args) != 2 {
+		fmt.Printf("%s <filepath>\n", os.Args[0])
+		fmt.Printf("  filepath - path to big endian ECOFF file.\n")
+		os.Exit(1)
+	}
+	path := os.Args[1]
 
-  file, err := os.Open(path)
-  if err != nil { panic(err) }
-  defer file.Close()
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-  fmt.Printf("%s opened\n", path)
-  header, err := ecoff.ParseHeader(file, binary.BigEndian)
-  if err != nil { panic(err) }
-  fmt.Printf("Parsed header from %s:\n%+v\n", path, header)
+	fmt.Printf("%s opened\n", path)
+	header, err := ecoff.ParseHeader(file, binary.BigEndian)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Parsed header from %s:\n%+v\n", path, header)
 }
--- file.go
+++ file.go
@@ -1,19 +1,19 @@
 package ecoff
 
 import (
-  "encoding/binary"
-  "io"
-  "fmt"
+	"encoding/binary"
+	"fmt"
+	"io"
 )
 
 type FileHeader struct {
-  Magic uint16
-  NumSections uint16
-  TimeDate int32
-  SymbolsPointer uint32
-  NumSymbols int32
-  OptionalHeader uint16
-  Flags uint16
+	Magic          uint16
+	NumSections    uint16
+	TimeDate       int32
+	SymbolsPointer uint32
+	NumSymbols     int32
+	OptionalHeader uint16
+	Flags          uint16
 }
 
 func (f FileHeader) String() string {
@@ -21,67 +21,67 @@ func (f FileHeader) String() string {
 }
 
 type ObjectHeader struct {
-  Magic int16
-  Vstamp int16
-  TextSize int32
-  DataSize int32
-  BssSize int32
-  Entry uint32
-  TextStart uint32
-  DataStart uint32
-  BssStart uint32
-  GprMask uint32
-  CprMask [4]uint32
-  GpValue uint32
+	Magic     int16
+	Vstamp    int16
+	TextSize  int32
+	DataSize  int32
+	BssSize   int32
+	Entry     uint32
+	TextStart uint32
+	DataStart uint32
+	BssStart  uint32
+	GprMask   uint32
+	CprMask   [4]uint32
+	GpValue   uint32
 }
 
 func (h ObjectHeader) String() string {
-  return fmt.Sprintf("{Magic: 0x%X, Vstamp: 0x%X, TextSize: %d, DataSize: %d, BssSize: %d, Entry: 0x%X, TextStart: 0x%X, DataStart: 0x%X, BssStart: 0x%X, GprMask: 0x%X, CprMask: 0x%X, GpValue: 0x%X}", h.Magic, h.Vstamp, h.TextSize, h.DataSize, h.BssSize, h.Entry, h.TextStart, h.DataStart, h.BssStart, h.GprMask, h.CprMask, h.GpValue)
+	return fmt.Sprintf("{Magic: 0x%X, Vstamp: 0x%X, TextSize: %d, DataSize: %d, BssSize: %d, Entry: 0x%X, TextStart: 0x%X, DataStart: 0x%X, BssStart: 0x%X, GprMask: 0x%X, CprMask: 0x%X, GpValue: 0x%X}", h.Magic, h.Vstamp, h.TextSize, h.DataSize, h.BssSize, h.Entry, h.TextStart, h.DataStart, h.BssStart, h.GprMask, h.CprMask, h.GpValue)
 }
 
 type SectionHeader struct {
-  Name [8]uint8
-  PhysicalAddress uint32
-  VirtualAddress uint32
-  Size int32
-  SectionPointer uint32
-  RelocationsPointer uint32
-  LnnoPtr int32
-  NumRelocations uint16
-  NumLnno uint16
-  Flags int32
+	Name               [8]uint8
+	PhysicalAddress    uint32
+	VirtualAddress     uint32
+	Size               int32
+	SectionPointer     uint32
+	RelocationsPointer uint32
+	LnnoPtr            int32
+	NumRelocations     uint16
+	NumLnno            uint16
+	Flags              int32
 }
 
 func (h SectionHeader) String() string {
-  return fmt.Sprintf("{Name: %s, PhysicalAddress: 0x%X, VirtualAddress: 0x%X, Size: %d, SectionPointer: 0x%X, RelocationsPointer: 0x%X, LnnoPtr: 0x%X, NumRelocations: %d, NumLnno: %d, Flags: 0x%X}", string(h.Name[:]), h.PhysicalAddress, h.VirtualAddress, h.Size, h.SectionPointer, h.RelocationsPointer, h.LnnoPtr, h.NumRelocations, h.NumLnno, h.Flags)
+	return fmt.Sprintf("{Name: %s, PhysicalAddress: 0x%X, VirtualAddress: 0x%X, Size: %d, SectionPointer: 0x%X, RelocationsPointer: 0x%X, LnnoPtr: 0x%X, NumRelocations: %d, NumLnno: %d, Flags: 0x%X}", string(h.Name[:]), h.PhysicalAddress, h.VirtualAddress, h.Size, h.SectionPointer, h.RelocationsPointer, h.LnnoPtr, h.NumRelocations, h.NumLnno, h.Flags)
 }
 
 type Header struct {
-  FileHeader FileHeader
-  ObjectHeader ObjectHeader
-  SectionHeaders []SectionHeader
+	FileHeader     FileHeader
+	ObjectHeader   ObjectHeader
+	SectionHeaders []SectionHeader
 }
 
 // Parse all headers out from a ECOFF file.
 func ParseHeader(r io.Reader, bo binary.ByteOrder) (Header, error) {
-  out := Header{}
-  err := binary.Read(r, bo, &out.FileHeader)
-  if err != nil {
-    return out, err
-  }
+	out := Header{}
+	err := binary.Read(r, bo, &out.FileHeader)
+	if err != nil {
+		return out, err
+	}
 
-  err = binary.Read(r, bo, &out.ObjectHeader)
-  if err != nil {
-    return out, err
-  }
+	err = binary.Read(r, bo, &out.ObjectHeader)
+	if err != nil {
+		return out, err
+	}
 
-  var i uint16
-  for i = 0; i < out.FileHeader.NumSections; i++ {
-    out.SectionHeaders = append(out.SectionHeaders, SectionHeader{})
-    err = binary.Read(r, bo, &out.SectionHeaders[i])
-    if err != nil {
-      return out, err
-    }
-  }
-  return out, nil
+	var i uint16
+	for i = 0; i < out.FileHeader.NumSections; i++ {
+		out.SectionHeaders = append(out.SectionHeaders, SectionHeader{})
+		err = binary.Read(r, bo, &out.SectionHeaders[i])
+		if err != nil {
+			return out, err
+		}
+	}
+	return out, nil
 }
